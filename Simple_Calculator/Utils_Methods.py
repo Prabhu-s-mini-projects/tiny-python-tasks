496d6b2505cc67dda906b88a6c2d6dedce919120
Prabhukumar Sivamoorthy <35177754+PrabhukumarSivamoorthy@users.noreply.github.com>
2024-08-04 18:27:23 -0700

    Version 1: Simple_Calculator Completed

---
+++ Simple_Calculator/Utils_Methods.py
@@ -0,0 +1,18 @@
+# for Add operation
+def add(number1, number2):
+    return number1 + number2
+
+
+# for Subtract
+def sub(number1, number2):
+    return number1 - number2
+
+
+# for Multiply operation
+def mul(number1, number2):
+    return number1 * number2
+
+
+# for division
+def div(number1, number2):
+    return number1 / number2
