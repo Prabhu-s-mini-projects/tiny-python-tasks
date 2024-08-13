b3bcdacb0dfa426598235b2abb1bb92ffe3a73b8
Prabhukumar Sivamoorthy <35177754+PrabhukumarSivamoorthy@users.noreply.github.com>
2024-08-13 09:30:52 -0700

    Version 1.1: Working as per the req.

--- Guess_the_Number/Guess_the_Number.py
+++ Guess_the_Number/Guess_the_Number.py
@@ -7,10 +7,13 @@ print(" Welcome to Guess the Number Game \n")
 difficulty_level = input("\n Choose the difficulty Level: \n'Easy'\n'Medium'\n'Hard'\n").lower()
 print(" I'm thinking of a number between 1 and 10.\n")
 
+# Fetch it from Db
 attempts = Db.number_of_attempts.get(difficulty_level)
-is_user_found = False
+
+# Computer will decide the number
 number = random.choice(range(1, 100))
 
+is_user_found = False # To keep track whether user found the value.
 while attempts > 0 and not is_user_found:
 
     guessed_number = int(input("\nYou Guess the Number:\t"))
