192a73d2bd7071e027fb97fa552803ca76029f0e
Prabhukumar Sivamoorthy <35177754+PrabhukumarSivamoorthy@users.noreply.github.com>
2024-09-06 01:22:55 -0700

    feat: Password_Manager dev completed.

--- Password_Manager/Password_Manager.py
+++ Password_Manager/Password_Manager.py
@@ -1,21 +1,48 @@
 # Dependencies
+import json
 from tkinter import *
 from tkinter import messagebox # message box is not a class. So we need to import  separately.
 import pyperclip
 
-# Internal modules
-from Controller.password_generator import PasswordManager
 
+# Internal modules
+from Controller.password_generator import PasswordGenerator
 
 # CONSTANTS
 LOGO_PATH = "Database/logo.png"
+
+# ---------------------------- Search  ------------------------------- #
+def search()-> None:
+
+    # Fetch the website name
+    website = entered_website.get()
+
+    try: # For the first time JSON file is not available or deleted
+
+        # reading the stored Passwords
+        with open("passwords.json",'r') as json_file:
+            data = json.load(json_file)
+
+    except FileNotFoundError:
+        messagebox.showinfo(title="NOT FOUND", message="Entered Website is not in database\n")
+
+    else:
+
+        if website.lower() in data.keys():
+            credentials = data.get(website.lower())
+            messagebox.showinfo(title=website, message="Here are the Credentials\n"
+                                                       f"Email : {credentials.get('email')}\n"
+                                                       f"password: {credentials.get('password')}")
+        else:
+            messagebox.showinfo(title="NOT FOUND", message="Entered Website is not in database\n")
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password()-> None:
 
     # Generate a password
-    password_manager = PasswordManager()
-    password = password_manager.generate()
+    password_generator = PasswordGenerator()
+    password = password_generator.generate()
 
     # Updating the password
     entered_password.insert(0,password)
@@ -28,9 +55,18 @@ def generate_password()-> None:
 
 def save()-> None:
 
+    # Fetching the data from the list
     password =  entered_password.get()
     website = entered_website.get()
     email = entered_email.get()
+
+    # Creating new entry in the form dict
+    new_data = {
+        website.lower():{
+            "email": email,
+            "password": password
+        }
+    }
 
     # will all user to save data only after all the details are entered
     if email and password and website:
@@ -43,9 +79,25 @@ def save()-> None:
 
         if is_ok:
 
-            # Save the data to the file
+            # Save the data to text file
             with open("passwords.txt","a") as file:
                 file.writelines(f"{website}| {email} | {password}\n")
+
+            try:
+                with open("passwords.json", 'r') as json_file:
+                    data = json.load(json_file)
+
+            except FileNotFoundError:
+                with open("passwords.json","w") as file:
+                    json.dump(new_data,file,indent=4)
+            else:
+
+                # updating the data
+                data.update(new_data)
+
+                # writing the data
+                with open("passwords.json", 'w') as json_file:
+                    json.dump(data,json_file,indent=4)
 
             # Deletes the data in the text box.
             entered_website.delete(0,END)
@@ -78,8 +130,8 @@ password_label = Label(text="Password:")
 password_label.grid(row=3,column=0)
 
 # Creating input textbox for website
-entered_website = Entry(width=38)
-entered_website.grid(row=1,column=1, columnspan=2)
+entered_website = Entry(width=21)
+entered_website.grid(row=1,column=1)
 
 # To keep the cursor in the textbox
 entered_website.focus()
@@ -94,6 +146,10 @@ entered_email.insert(0,"prabhu@gmail.com")
 # Creating input textbox for password
 entered_password = Entry(width=21)
 entered_password.grid(row=3, column=1)
+
+# Creating a search Button
+search_button = Button(text="Search",command=search,width=13)
+search_button.grid(row=1,column=2)
 
 # Creating a generate Button
 generate_button = Button(text="Generate Password",command=generate_password)
