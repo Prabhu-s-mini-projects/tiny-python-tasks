ef21d5c490ff3cf22ef0a590900ee84b9ebea156
Prabhukumar Sivamoorthy <35177754+PrabhukumarSivamoorthy@users.noreply.github.com>
2024-09-23 11:10:22 -0700

    fix: pylint fixes

--- Tasks/Bid_Auction/bid_acution.py
+++ Tasks/Bid_Auction/bid_acution.py
@@ -23,42 +23,45 @@ def add_new_bidder(bidder_name, bidders_amount):
     }
     bidders.append(new_bidder)
 
+def main()-> None:
+    bid_window_open:bool = True
+    while bid_window_open:
+        # Get Bidder Information
+        name = input("Enter your name:\t").lower()
+        bid_amount = int(input("Enter your BID_amount $:\t"))
 
-bid_window_open = True
-while bid_window_open:
-    # Get Bidder Information
-    name = input("Enter your name:\t").lower()
-    bid_amount = int(input("Enter your BID_amount $:\t"))
+        # Logging all the Bidders info
+        add_new_bidder(name, bid_amount)
 
-    # Logging all the Bidders info
-    add_new_bidder(name, bid_amount)
+        # Making sure Everyone placed a BID.
+        other_bidders = input("Are there are any other bidders?"
+                              "\tType 'yes' to continue or 'any key' for no\n").lower()
 
-    # Making sure Everyone placed a BID.
-    other_bidders = input("Are there are any other bidders?"
-                          "\tType 'yes' to continue or 'any key' for no\n").lower()
+        if other_bidders == "yes":
+            # Set the TERM environment variable
+            os.environ['TERM'] = 'xterm-256color'
 
-    if other_bidders == "yes":
-        # Set the TERM environment variable
-        os.environ['TERM'] = 'xterm-256color'
+            # clear the screen so there won't see previous person's BID
+            os.system('clear')
 
-        # clear the screen so there won't see previous person's BID
-        os.system('clear')
+        else:
+            bid_window_open = False
 
-    else:
-        bid_window_open = False
+    # Finding the MAX bid amount and the bidder
+    max_bid:int = 0
+    leading_bidder: str = 'House'
+    for bidder in bidders:
+        if bidder['bid_amount'] > max_bid:
+            max_bid = bidder['bid_amount']
+            leading_bidder = bidder['name']
+        # added this condition to handle the same amount bid
+        elif bidder['bid_amount'] == max_bid and max_bid > 0:
+            max_bid = bidder['bid_amount']
+            leading_bidder += ' + ' + bidder['name']
+        else:
+            pass
 
-# Finding the MAX bid amount and the bidder
-max_bid = 0
-leading_bidder = 'House'
-for bidder in bidders:
-    if bidder['bid_amount'] > max_bid:
-        max_bid = bidder['bid_amount']
-        leading_bidder = bidder['name']
-    # added this condition to handle the same amount bid
-    elif bidder['bid_amount'] == max_bid and max_bid > 0:
-        max_bid = bidder['bid_amount']
-        leading_bidder += ' + ' + bidder['name']
-    else:
-        pass
+    print(f"{leading_bidder} WON the BID-AUCTION with the BID_amount: {max_bid}")
 
-print(f"{leading_bidder} WON the BID-AUCTION with the BID_amount: {max_bid}")
+if "__name__" == "__main__":
+    main()
