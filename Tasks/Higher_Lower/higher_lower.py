a51475888cd9d8484d9f92a625a8f91662815c26
Prabhukumar Sivamoorthy <35177754+PrabhukumarSivamoorthy@users.noreply.github.com>
2024-09-25 10:04:52 -0700

    fix: pylint fixes

---
+++ Tasks/Higher_Lower/higher_lower.py
@@ -0,0 +1,46 @@
+"""main script identify higher and lower """
+import random
+import Higher_Lower_Database as Db
+
+def compare(compare_a: int, compare_b: int, user_input: str) -> bool:
+    """ identify which one is greater returns true or false based on user input """
+    return (compare_a >= compare_b and user_input == 'a') or (compare_b >= compare_a and user_input == 'b')
+
+def main()-> None:
+    """start of program"""
+
+    # Welcome logo
+    print(Db.logo)
+
+    score = 0
+
+    # Randomly selecting a person from the game database
+    compare_b = random.choice(Db.data)
+
+    # Keep the loop running until user input is wrong
+    continue_game = True
+    while continue_game:
+
+        compare_a = compare_b
+        print(f"Compare A : {compare_a.get('name')},{compare_a.get('description')},{compare_a.get('country')}.")
+
+        print(Db.vs)
+
+        compare_b = random.choice(Db.data)
+        print(f"Compare B : {compare_b.get('name')},{compare_b.get('description')},{compare_b.get('country')}.")
+
+        answer = input("Who has more followers?  Type 'A' or 'B' : \t").lower()
+
+        continue_game = compare(compare_a.get('follower_count'), compare_b.get('follower_count'), answer)
+        if continue_game:
+            score += 1
+            print("Correct :-) !")
+        else:
+            print("GAME OVER :-( !")
+
+        print(f"{compare_a.get('name')} has {compare_a.get('follower_count')} Million followers,")
+        print(f"{compare_b.get('name')} has {compare_b.get('follower_count')} Million followers,")
+        print(f"Your score is {score}.")
+
+if __name__ == '__main__':
+    main()
