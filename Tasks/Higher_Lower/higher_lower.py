15d36d25200d869df25549a9cf74314151b02d1d
Prabhukumar Sivamoorthy <35177754+PrabhukumarSivamoorthy@users.noreply.github.com>
2024-09-25 10:13:34 -0700

    fix: pylint fixes

--- Tasks/Higher_Lower/higher_lower.py
+++ Tasks/Higher_Lower/higher_lower.py
@@ -4,7 +4,8 @@ import higher_lower_database as db
 
 def compare(compare_a: int, compare_b: int, user_input: str) -> bool:
     """ identify which one is greater returns true or false based on user input """
-    return (compare_a >= compare_b and user_input == 'a') or (compare_b >= compare_a and user_input == 'b')
+    return ((compare_a >= compare_b and user_input == 'a')
+            or (compare_b >= compare_a and user_input == 'b'))
 
 def main()-> None:
     """start of program"""
@@ -22,16 +23,19 @@ def main()-> None:
     while continue_game:
 
         compare_a = compare_b
-        print(f"Compare A : {compare_a.get('name')},{compare_a.get('description')},{compare_a.get('country')}.")
+        print(f"Compare A : {compare_a.get('name')},"
+              f"{compare_a.get('description')},{compare_a.get('country')}.")
 
         print(db.VS)
 
         compare_b = random.choice(db.data)
-        print(f"Compare B : {compare_b.get('name')},{compare_b.get('description')},{compare_b.get('country')}.")
+        print(f"Compare B : {compare_b.get('name')},"
+              f"{compare_b.get('description')},{compare_b.get('country')}.")
 
         answer = input("Who has more followers?  Type 'A' or 'B' : \t").lower()
 
-        continue_game = compare(compare_a.get('follower_count'), compare_b.get('follower_count'), answer)
+        continue_game = compare(compare_a.get('follower_count'),
+                                compare_b.get('follower_count'), answer)
         if continue_game:
             score += 1
             print("Correct :-) !")
