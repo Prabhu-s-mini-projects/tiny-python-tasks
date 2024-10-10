dcec4e4703154b83b4bcb5ba4954336ce31a6123
Prabhukumar Sivamoorthy <35177754+PrabhukumarSivamoorthy@users.noreply.github.com>
2024-10-10 08:54:50 -0700

    feat: sheety api is working as per the expectation

--- Tasks/Flight_Tracker/flight_search.py
+++ Tasks/Flight_Tracker/flight_search.py
@@ -5,4 +5,11 @@
 
 class FlightSearch:
     """This class is responsible for talking to the Flight Search API."""
-    pass
+
+    def __init__(self):
+        print("cTodod")
+
+    def get_destination_code(self, city_name):
+        # Return "TESTING" for now to make sure Sheety is working. Get TEQUILA API data later.
+        code = "TESTING"
+        return code
