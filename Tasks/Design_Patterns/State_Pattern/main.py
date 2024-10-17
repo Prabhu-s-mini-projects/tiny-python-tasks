04da45ef02d598dc1acedc1f4e14ab26933f33c9
Prabhukumar Sivamoorthy <35177754+PrabhukumarSivamoorthy@users.noreply.github.com>
2024-10-16 23:51:04 -0700

    feat: state Design Pattern

---
+++ Tasks/Design_Patterns/State_Pattern/main.py
@@ -0,0 +1,22 @@
+"""
+App_Name: state Pattern
+Purpose: Example of a state-pattern
+"""
+
+
+# Internal modules
+
+
+# Methods-------------------------------------------------------------------
+
+def main() -> None:
+    """
+    Here is the Starting point of an App : state Pattern
+    to do Example of a state pattern
+    """
+    # To do
+    print("states")
+
+
+if __name__ == '__main__':
+    main()
