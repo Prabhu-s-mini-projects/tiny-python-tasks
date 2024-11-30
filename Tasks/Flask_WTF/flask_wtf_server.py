dde2fd604c5cb8f0673525059de0cc07023501da
Prabhukumar Sivamoorthy <35177754+PrabhukumarSivamoorthy@users.noreply.github.com>
2024-11-30 09:52:00 -0800

    fix: pyl

--- Tasks/Flask_WTF/flask_wtf_server.py
+++ Tasks/Flask_WTF/flask_wtf_server.py
@@ -4,6 +4,9 @@ purpose: example of using the Flask WTForms
 """
 # Dependencies
 from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email
 from flask_bootstrap import Bootstrap5
 
 # Internal Modules
@@ -11,12 +14,11 @@ app = Flask(__name__)
 app.secret_key = "some secret string"
 bootstrap = Bootstrap5(app)
 
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email
-
 
 class MyForm(FlaskForm):
+    """
+    Creates a blueprint of a form
+    """
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField(label='log in')
@@ -40,8 +42,7 @@ def login() -> str:
     if form.validate_on_submit() and request.method == 'POST':
         if form.email.data == "fname.lname@abc.com" and form.password.data == "12345678":
             return render_template('success.html', form=form)
-        else:
-            return render_template('denied.html', form=form)
+        return render_template('denied.html', form=form)
     return render_template('login.html', form=form)
 
 
