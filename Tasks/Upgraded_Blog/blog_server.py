e512166a985821e602bf4db29201f319d90fe418
Prabhukumar Sivamoorthy <35177754+PrabhukumarSivamoorthy@users.noreply.github.com>
2024-12-04 07:17:18 -0800

    feat: "section 59: Upgraded Blog"

--- Tasks/Upgraded_Blog/blog_server.py
+++ Tasks/Upgraded_Blog/blog_server.py
@@ -3,6 +3,7 @@ AppName:Blog_server
 purpose: will act as a server for upgraded blog
 """
 # Dependencies
+import requests
 from flask import Flask, render_template
 
 # Internal Modules
@@ -17,6 +18,39 @@ def home() -> str:
     """
     return render_template('index.html')
 
+
+@app.route('/contact')
+def contact():
+    """Takes you to the contact page"""
+    return render_template('contact.html')
+
+
+@app.route('/about')
+def about():
+    """Takes you to the contact page"""
+    return render_template('about.html')
+
+
+@app.route('/post')
+def post():
+    """Takes you to post page"""
+    response = requests.get(url="https://api.npoint.io/64c4c05d34812b411e06", timeout=10)
+    posts = response.json()
+    return render_template(
+        'post.html',
+        posts=posts
+    )
+
+
+@app.route('/post/<p_id>')
+def show_post(p_id: int):
+    """Takes you to post page"""
+    response = requests.get(url="https://api.npoint.io/64c4c05d34812b411e06", timeout=10)
+    i_post = response.json()[int(p_id) - 1]
+    return render_template(
+        'show_post.html',
+        post=i_post
+    )
 
 # ------------------------------------------
 if __name__ == "__main__":
